dbe9de09585832ee8040584b12a6121ba968953e
Christine Mumbi <christinemumbi286@gmail.com>
2025-11-23 12:56:14 +0300

    made changes on order file

--- order.py
+++ order.py
@@ -1,16 +1,19 @@
 class Order:
-    # Validates the customer is an instance of Customer
-    if not isinstance(customer, Customer):
-        raise ValueError("Invalid customer information.")
-
-    # checks that every item in the coffee_items list is an instance of Coffee
-    for item in coffee_items:
-        if not isinstance(item, Coffee):
-            raise ValueError("Invalid coffee item in the order.")
-            
-            # If validations pass, initialize the order
     def __init__(self, customer, coffee_items):
+
+        # validates the customer is an instance of Cusotomer
+        if not isinstance(customer, Customer):
+            raise ValueError("customer must be an instance of Customer class")
+
+        # Checks that every item in coffee_items is an instance of Coffee
+        for item in coffee_items:
+            if not isinstance(item, Coffee):
+                raise ValueError("All items in coffee_items must be instances of Coffee class")
         self.customer = customer
-        self.coffee_items = coffee_items  # List of Coffee objects
+        self.coffee_items = coffee_items #list of Coffe objects
+
+        # Calculates total price of the order
+        def total_price(self):
+            return sum(item.price for item in self.coffee_items)
         
     pass
